e583b239cc84ac760030e04a68b4f29a5032813d
lyj <814200246@qq.com>
2018-11-17 16:38:20 +0800

    配置基本测试

--- jeefast-system/src/main/java/cn/jeefast/JeeFastSystemApplication.java
+++ jeefast-system/src/main/java/cn/jeefast/JeeFastSystemApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(value = "cn.jeefast")
 @SpringBootApplication
 public class JeeFastSystemApplication extends SpringBootServletInitializer {
 
