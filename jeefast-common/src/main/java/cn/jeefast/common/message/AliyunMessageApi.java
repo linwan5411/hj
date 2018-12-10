cf611e1e22cec3fa379f40da83f80626a1e01631
linwan5411 <814200246@qq.com>
2018-12-10 22:28:43 +0800

    接口完成第一次提及

--- jeefast-common/src/main/java/cn/jeefast/common/message/AliyunMessageApi.java
+++ jeefast-common/src/main/java/cn/jeefast/common/message/AliyunMessageApi.java
@@ -29,8 +29,8 @@ public class AliyunMessageApi {
     private static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    private static final String accessKeyId = "";
-    private static final String accessKeySecret = "";
+    private static final String accessKeyId = "LTAI4RZIgFWrqueZ";
+    private static final String accessKeySecret = "J942FHGgzbIpyeBdsaVKszCAqKhMbE";
 
     //必填:短信签名-可在短信控制台中找到
     private static final String signName = "";
